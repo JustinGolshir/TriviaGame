baa319033cacdc667433fa2732d469d76456fb33
justin <justin@tcadmins-MacBook-Pro-59.local>
2018-03-27 15:18:09 -0400

    bug fixes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,63 @@
 
-    $('#start').on('click', function () {
+    $('#start').on('click', function (){
         game.start();
     })
-   
+
+    $(document).on('click', '#end', function(){
+        game.over();
+    })
+
+var game = {
+    correct: 0,
+    incorrect: 0, 
+    counter: 120, 
+    countdown: function(){
+        game.counter--;
+        $('#counter').html(game.counter);
+        if(game.counter<=0){
+            console.log("Game over!");
+            game.over();
+        }
+    },
+    start: function(){
+        timer = setInterval(game.countdown,1000);
+        $('#subwrapper').prepend('<h2>Time Remaining: <span id="counter">120</span> Seconds</h2>');
+        $('#start').remove();
+        for(var i=0;i<questions.length;i++){
+            $('#subwrapper').append('<h2>'+questions[i].question+'</h2>');
+            for(var j=0;j<questions[i].choices.length;j++) {
+                $('#subwrapper').append(" <input type='radio' name='question-" +i+ "' value='" +questions[i].choices[j]+"'> " +questions[i].choices[j])
+            }
+        }
+    },
+    over: function(){
+        for (i = 0; i < questions.length; i++) { 
+            var selector = "input[name='question-" +i+ "']:checked";
+            $.each($(selector), function(){
+                if($(selector).val()==questions[i].correctAnswer){
+                    game.correct++
+                } else {
+                    game.incorrect++;
+                }
+            });
+        }
+
+        this.result();
+        },
+
+        result: function(){
+            clearInterval(timer);
+            $('#subwrapper h2').remove();
+
+            $('#subwrapper').html("<h2>All done!</h2>");
+            $('#subwrapper').append("<h3>Correct Answers: "+this.correct+"</h3>");
+            $('#subwrapper').append("<h3>Incorrect Answers: "+this.incorrect+"</h3>");
+            $('#subwrapper').append("<h3>Unanswered: "+(questions.length-(this.incorrect+this.correct))+"</h3>");
+        }
+    }
+
+// Game Questions and Answers
+
     var questions = [{
         question: "In the year 1900 in the U.S. what were the most popular first names given to boy and girl babies?",
         choices: ["William and Elizabeth", "Joseph and Catherine", "John and Mary", "George and Anne"],
@@ -36,98 +91,3 @@
         choices: ["8", "18", "38", "58"],
         correctAnswer: "18"
 }];
-
-var game = {
-    correct: 0,
-    incorrect: 0, 
-    counter: 5, 
-    countdown: function(){
-        game.counter--;
-        $('#counter').html(game.counter);
-        if(game.counter<=0){
-            console.log("Game over!");
-            game.done();
-        }
-    },
-    start: function(){
-        timer = setInterval(game.countdown,1000);
-        $('#subwrapper').prepend('<h2>Time Remaining: <span id="counter">120</span> Seconds</h2>');
-        $('#start').remove();
-        for(var i=0;i<questions.length;i++){
-            $('#subwrapper').append('<h2>'+questions[i].question+'</h2>');
-            for(var j=0;j<questions[i].choices.length;j++) {
-                $('#subwrapper').append("<input type='radio' name='question-" +i+ "' value='" +questions[i].choices[j]+"'>" +questions[i].choices[j])
-            }
-        }
-    },
-    done: function(){
-        $.each($('input[name="question-0]":checked'), function(){
-            if($(this).val()==questions[0].correctAnswer){
-                game.correct++
-            } else {
-                game.incorrect++;
-            }
-        });
-        $.each($('input[name="question-1]":checked'), function(){
-            if($(this).val()==questions[1].correctAnswer){
-                game.correct++
-            } else {
-                game.incorrect++;
-            }
-        });
-        $.each($('input[name="question-2]":checked'), function(){
-            if($(this).val()==questions[2].correctAnswer){
-                game.correct++
-            } else {
-                game.incorrect++;
-            }
-        });
-        $.each($('input[name="question-3]":checked'), function(){
-            if($(this).val()==questions[3].correctAnswer){
-                game.correct++
-            } else {
-                game.incorrect++;
-            }
-        });
-        $.each($('input[name="question-4]":checked'), function(){
-            if($(this).val()==questions[4].correctAnswer){
-                game.correct++
-            } else {
-                game.incorrect++;
-            }
-        });
-        $.each($('input[name="question-5]":checked'), function(){
-            if($(this).val()==questions[5].correctAnswer){
-                game.correct++
-            } else {
-                game.incorrect++;
-            }
-        });
-        $.each($('input[name="question-6]":checked'), function(){
-            if($(this).val()==questions[6].correctAnswer){
-                game.correct++
-            } else {
-                game.incorrect++;
-            }
-        });
-        $.each($('input[name="question-7]":checked'), function(){
-            if($(this).val()==questions[7].correctAnswer){
-                game.correct++
-            } else {
-                game.incorrect++;
-            }
-        });
-
-        this.result();
-        },
-
-        result: function(){
-            clearInterval(timer);
-            $('#subwrapper h2').remove();
-
-            $('#subwrapper').html("<h2>All done!</h2>");
-            $('#subwrapper').append("<h3>Correct Answers: "+this.correct+"</h3>");
-            $('#subwrapper').append("<h3>Incorrect Answers: "+this.incorrect+"</h3>");
-            $('#subwrapper').append("<h3>Unanswered: "+(questions.length-(this.incorrect+this.correct))+"</h3>");
-        }
-    }
